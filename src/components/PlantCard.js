6cb32b7b99ff89396d5d0b2db658766b23e656f3
Jenni Rivera <jenninrivera@gmail.com>
2023-08-29 10:19:59 -0400

    Complete Lab

--- src/components/PlantCard.js
+++ src/components/PlantCard.js
@@ -1,16 +1,24 @@
-import React from "react";
+import React, {useState} from "react";
 
-function PlantCard() {
+function PlantCard({plant, handleSoldOut}) {
+  const [inStock, setInStock] = useState(true)
+
+  function handleSoldOut() {
+    setInStock(!inStock) 
+  }
+  
   return (
     <li className="card">
-      <img src={"https://via.placeholder.com/400"} alt={"plant name"} />
-      <h4>{"plant name"}</h4>
-      <p>Price: {"plant price"}</p>
-      {true ? (
+      <img src={plant.image} alt={plant.name} />
+      <h4>{plant.name}</h4>
+      <p>Price: {plant.price}</p>
+      <button onClick={handleSoldOut}>
+        {inStock ? (
         <button className="primary">In Stock</button>
       ) : (
         <button>Out of Stock</button>
       )}
+        </button>
     </li>
   );
 }
--- src/components/PlantPage.js
+++ src/components/PlantPage.js
@@ -2,13 +2,43 @@ import React from "react";
 import NewPlantForm from "./NewPlantForm";
 import PlantList from "./PlantList";
 import Search from "./Search";
+import { useEffect, useState } from "react"
 
 function PlantPage() {
+  const [plants, setPlants] = useState ([])
+  const [plantSearch, setPlantSearch] = useState("")
+useEffect(() => {
+  fetch('http://localhost:6001/plants')
+  .then(response => response.json())
+  .then(plants => setPlants(plants))
+}, [])
+const addNewPlant = event => {
+  event.preventDefault()
+  const newPlant = {
+      "name": event.target.name.value,
+      "image": event.target.image.value,
+      "price": event.target.price.value
+  }
+  fetch('http://localhost:6001/plants', {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept'": "application/json"
+    },
+    body: JSON.stringify(newPlant)
+  })
+  setPlants([...plants, newPlant])
+}
+const updatePlantSearch = event => setPlantSearch(event.target.value)
+
+const filteredPlants = plants.filter(plant => {
+  return (plantSearch ? plant.name.toUpperCase().includes(plantSearch.toUpperCase()): true)
+})
   return (
     <main>
-      <NewPlantForm />
-      <Search />
-      <PlantList />
+      <NewPlantForm addNewPlant={addNewPlant}/>
+      <Search updatePlantSearch={updatePlantSearch}/>
+      <PlantList plants={filteredPlants}/>
     </main>
   );
 }
